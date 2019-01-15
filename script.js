4cf07d2d7e5dfa6b385658d21d202ae1e2853936
Glitch (hello-webpage) <none>
2019-01-15 19:57:30 +0000

    fetch and promises

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-var index = ["https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json", 
-             "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json"],
-    proms = index.map(d => fetch(d));
+/* globals d3 */
+
+// Stack Overflow: How to use multiple XMLHttpRequest?
+// Answer: https://stackoverflow.com/a/46505251
+const index = ["https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json", 
+             "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json"]
+const proms = index.map(d => fetch(d));
+let data = [];
 
 Promise.all(proms)
        .then(ps => Promise.all(ps.map(p => p.json()))) // p.json() also returns a promise
-       .then(js => js.forEach((j,i) => (console.log(`RESPONSE FOR: ${index[i]}:`), console.log(j))));
+       .then(js => js.forEach((j,i) => (data.push(j))))
+       .then(console.log(data));
