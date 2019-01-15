1949fd40217713895e6c2116ee601b47854fa5ff
Glitch (hello-webpage) <none>
2019-01-15 23:08:11 +0000

    😅🚜 Checkpoint ./index.html:2265073/316 ./script.js:2265073/2004

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* globals d3 */
+/* globals d3, topojson */
 
 // Stack Overflow: How to use multiple XMLHttpRequest?
 // Answer: https://stackoverflow.com/a/46505251
@@ -18,7 +18,7 @@ Promise.all(proms)
     let nationPath = "";
    
     console.log(dataset2);
-    //console.log(nation);
+    console.log(counties);
     
     for(let i = 0; i < nation.length; i++) {
       if(i === 0) {
@@ -29,10 +29,9 @@ Promise.all(proms)
     }
     
     //console.log(nationPath);
-    
      
     const w = 1200
-    const h = 400;
+    const h = 700;
   
     dataset1.map((d, i) => d.polygon = counties[i].arcs);
     console.log(dataset1);
@@ -48,10 +47,17 @@ Promise.all(proms)
     console.log(path);
   
     const svg = d3.select(".container")
-        .append("svg")
-        .attr("id", "chart")
-        .attr("width", w)
-        .attr("height", h)
+      .append("svg")
+      .attr("id", "chart")
+      .attr("width", w)
+      .attr("height", h)
+    
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .attr("id", "tooltip")
+      .style("opacity", 0)
 
     d3.select(".heading")
       .append("h1")
@@ -63,21 +69,32 @@ Promise.all(proms)
       .attr("id", "description")
       .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
     
-    /*svg.append("path")
-          .attr("path", nationPath)
-          .style("stroke", "#fff")
-          .style("stroke-width", "1")*/
-    
     svg.selectAll("path")
-          .data(topojson.feature(us, us.objects.counties).features)
-          .enter()
-          .append("path")
-          .attr("class", "county")
-          .attr("data-fips", d => d.fips)
-          .attr("data-education", d => d.bachelorsOrHigher)
-          .attr("d", "M 10 25 L 10 75 L 60 75 L 10 25")
-          .style("stroke", "#fff")
-          .style("stroke-width", "1")
+      .data(topojson.feature(dataset2, dataset2.objects.counties).features)
+      .enter()
+      .append("path")
+      .attr("class", "county")
+      .attr("data-fips", (d,i) => dataset1[i].fips)
+      .attr("data-education", (d,i) => dataset1[i].bachelorsOrHigher)
+      .attr("d", path)
+      .attr("fill", d)
+      .on("mouseover", function(d,i) {
+          tooltip
+            .transition()
+            .duration(100)
+            .style("opacity", 0.85);
+          tooltip
+            .html("<p>" + dataset1[i].bachelorsOrHigher + "</p>")
+            .style("left", d3.event.pageX + 15 + "px")
+            .style("top", d3.event.pageY + 15 + "px");
+          tooltip.attr("data-year", d.year);
+        })
+        .on("mouseout", function(d) {
+          tooltip
+            .transition()
+            .duration(100)
+            .style("opacity", 0);
+        });
     
     const legend = svg.append("g")
                     .attr("id", "legend");
