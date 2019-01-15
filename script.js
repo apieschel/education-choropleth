fcc1a560ce122bfb0dd5dc2bb744354f1d534bd7
Glitch (hello-webpage) <none>
2019-01-15 19:52:55 +0000

    🔫🌇 Checkpoint ./script.js:2265073/3794

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
-/* globals d3 */
-const xhr = new XMLHttpRequest();
-const callback = function(err, data) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  } else {
-    const dataset = data;
-    console.log(dataset);
-    /*
-    const xhr2 = new XMLHttpRequest();
-    const callback2 = function(err, data) {
-      if (err !== null) {
-        alert('Something went wrong: ' + err);
-      } else {
-        const dataset2 = data;
-        console.log(dataset2);
-      }
-    }
-    xhr2.open('GET', 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json', true);
-    xhr2.responseType = 'json';
-    xhr2.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
-    xhr2.send();
-    */
-  }
-}
+var index = ["https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json", 
+             "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json"],
+    proms = index.map(d => fetch(d));
 
-xhr.open('GET', 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json', true);
-xhr.responseType = 'json';
-xhr.onload = function() {
-  var status = xhr.status;
-  if (status === 200) {
-    callback(null, xhr.response);
-  } else {
-    callback(status, xhr.response);
-  }
-};
-xhr.send();
+Promise.all(proms)
+       .then(ps => Promise.all(ps.map(p => p.json()))) // p.json() also returns a promise
+       .then(js => js.forEach((j,i) => (console.log(`RESPONSE FOR: ${index[i]}:`), console.log(j))));
